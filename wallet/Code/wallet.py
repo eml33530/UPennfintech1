310a7211023bc69dd18e9fca2ef93ab7cd90d7bb
Eric Linn <eric.m.linn75@gmail.com>
2021-06-29 18:28:18 -0400

    final1

--- wallet/Code/wallet.py
+++ wallet/Code/wallet.py
@@ -94,4 +94,17 @@ def send_txn(coin,account,recipient, amount):
         print(signed_txn)
         return NetworkAPI.broadcast_tx_testnet(signed_txn)
 
+# Test transaction
 
+
+btc_acc = priv_key_to_account(BTCTEST,btc_PrivateKey)
+create_tx(BTCTEST,btc_acc,"mzAVYRCct8qM4pKbUDPr6JJZeC3vsEDMsz", 0.1)
+send_txn(BTCTEST,btc_acc,"mzAVYRCct8qM4pKbUDPr6JJZeC3vsEDMsz", 0.1)
+
+#ETH Transaction
+w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545/0x662541feec10852e7ec7179ad607366c51993142c467765223ba3f7715aed1b3"))
+w3.isConnected()
+w3.eth.getBalance("0x1445a2385fF2cd7A116F30a9d7d4C4871aA57D3a")
+create_tx(ETH,eth_acc,"0x1445a2385fF2cd7A116F30a9d7d4C4871aA57D3a", 1000)
+send_txn(ETH, eth_acc,"0x1445a2385fF2cd7A116F30a9d7d4C4871aA57D3a", 1000)
+w3.eth.getBalance("0x1445a2385fF2cd7A116F30a9d7d4C4871aA57D3a")
